2c7b67e3c4e752bc31d913decaf8650cd450a212
baobacluc3 <baobacluc3@gmail.com>
2026-06-18 16:50:11 +0700

    update

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,8 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
-//Request → Guard → JwtStrategy → validate user → attach to request.user
-
-//no token - reject request
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {}
